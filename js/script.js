2680d775874fe9d3485e0115eac8d4fae59753c6
Nathan Collinson <collinnath1@ecs.vuw.ac.nz>
2023-09-05 15:21:47 +1200

    add displays for the projects

--- js/script.js
+++ js/script.js
@@ -53,29 +53,31 @@ function loadProjects() {
         new Project("Colour Switcher", "colourSwitcher", "Colour Switcher was a Processing game I made for my Computer Graphics Course. "+
         "I largely based it off previous games I had played such as Colour Switch and Undertale. This was the first Processing game I had made, "+
         "which is a language that acts similarily to Java. I had to plan my game so that it would fit a theme and "+
-        "consist of different game modes. On my GitHub you can download the processing game.", ["img/chaps-challenge"])
+        "consist of different game modes. On my GitHub you can download the processing game <a href = 'https://github.com/NathanCollinson123'>here</a>.", ["img/colour-switcher"])
     );
         
 
     projectSearch.projects.push(
-        new Project("Chaps Challenge", "chapsChallenge", "Chaps Challenge was group project developed in java. My role in this project was to code the"+
-        "logic and main structure of the code. I had to work with the other members closely to "+
-        "communicate the structure of the code that where I used existing java patterns. I was quite "+
-        "proud with the game created and learnt a lot from managing roles within a group.", ["img/chaps-challenge"])
+        new Project("Personal Portfolio", "website", "I used my Personal Portfoio to showcase my projects and to share more about me. "+
+        "I planned the design of the website using wireframes, and have used GitHub to host and manage the version control of my site."+
+        " Currently this site is the first version, and I have made designs for my second site with Declan Cross. Working on this project" +
+        " taught me to learn javascript and some Jquery, and further onto the development I could add new javascript techniques I learnt" +
+        " from my courses at University.", ["img/website"])
     );
 
     projectSearch.projects.push(
-        new Project("Chaps Challenge", "chapsChallenge", "Chaps Challenge was group project developed in java. My role in this project was to code the"+
-        "logic and main structure of the code. I had to work with the other members closely to "+
-        "communicate the structure of the code that where I used existing java patterns. I was quite "+
-        "proud with the game created and learnt a lot from managing roles within a group.", ["img/chaps-challenge"])
+        new Project("Data Recorder", "dataRecorder", "Data Recorder was a large group project that I was part of for my Project Management Course at University."+
+        " The Data Recorder is a ESP32 board used to scan water levels and rain levels using the attached sensors and data that can be read from a web app." +
+        " For this project I was part of the embedded sub-group, which managed the main running functions with micropython." +
+        " This project taught me to work in a team environment, using project management methodologies and frameworks to plan out and deliver an outcome.", ["img/data-recorder"])
     );
 
     projectSearch.projects.push(
-        new Project("Chaps Challenge", "chapsChallenge", "Chaps Challenge was group project developed in java. My role in this project was to code the"+
-        "logic and main structure of the code. I had to work with the other members closely to "+
-        "communicate the structure of the code that where I used existing java patterns. I was quite "+
-        "proud with the game created and learnt a lot from managing roles within a group.", ["img/chaps-challenge"])
+        new Project("Realm Render", "realmRender", "Realm Render is a Dungeon's and Dragon map generator that is made with React."+
+        " This was a student led, group project would require us to plan our project entirely over the course of 12 weeks." +
+        " This project has taught me to self learn. Many of the tools in this project I would research and discover"+
+        " the processes and tools."+
+        " Currently this project is ongoing, so feel free to ask me questions about it.", ["img/realm-render"])
     );
     initProjectController(); // Call the function to initialize event listeners
     //generateProjectCarousel();
@@ -147,7 +149,7 @@ function handleProjectClick(event) {
     document.querySelectorAll(".selected").forEach(item => {item.classList.remove("selected");});
     target.classList.add("selected");
     document.getElementById("projectName").innerText = project.title;
-    document.getElementById("projectDescription").innerText = project.desc;
+    document.getElementById("projectDescription").innerHTML = project.desc;
 }
 /*
 // Wrap your event listener code in a function
